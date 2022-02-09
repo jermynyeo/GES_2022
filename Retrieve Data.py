1f7ed1ab475b434a2c619a0c751877c6f016cd7a
Jermyn Yeo <42236761+jermynyeo@users.noreply.github.com>
2022-02-09 12:45:49 +0800

    update scraping and SMU data

--- Retrieve Data.py
+++ Retrieve Data.py
@@ -8,14 +8,20 @@ pd.set_option('display.max_columns', None)
 
 df = pd.DataFrame()
 for year in range(2013, 2020): 
-    r = requests.get("https://data.gov.sg/api/action/datastore_search?resource_id=9326ca53-9153-4a9c-b93f-8ae032637b70&q=" + str(year))
-    data = r.json()['result']
+    url_base = "https://data.gov.sg"
+    endpoint = "/api/action/datastore_search?resource_id=9326ca53-9153-4a9c-b93f-8ae032637b70&q=" + str(year)
 
-    temp_df = pd.json_normalize(data, ["records"])
-    temp_df.drop(columns=["_full_count"], inplace=True)
-    temp_df.set_index('_id', inplace=True, drop=True)
-    df = df.append(temp_df)
-    
+    while (True):     
+        r = requests.get(url_base + endpoint)
+        data = r.json()['result']
+        if (data['records'] == []): 
+            break
+        endpoint = data['_links']['next']
+        temp_df = pd.json_normalize(data, ["records"])
+        temp_df.drop(columns=["_full_count"], inplace=True)
+        temp_df.set_index('_id', inplace=True, drop=True)
+        df = df.append(temp_df)
+
 df.to_csv("data/GES_2013_2019.csv")
 
 # files = glob.glob('data/*.pdf')
