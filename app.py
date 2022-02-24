fa1a1bc84528fd44a84e07691f14d5f65ba9916b
jimmeng-kok-2017 <43122778+jimmeng-kok-2017@users.noreply.github.com>
2022-02-24 20:39:18 +0800

    Update app.py

--- app.py
+++ app.py
@@ -24,16 +24,17 @@ try_school_uni_mean_yoy_basic_income_df.school = try_school_uni_mean_yoy_basic_i
 
 st.title("GES 2013 to 2020")
 
-search_text = st.text_input('Term to search (by degree):', '')
+search_uni_text = st.text_input('Term to search (by university):', '')
+search_degree_text = st.text_input('Term to search (by degree):', '')
 
-if search_text:
+if search_degree_text:
     query_df = try_school_uni_mean_yoy_basic_income_df[
-        try_school_uni_mean_yoy_basic_income_df.school.str.contains(f'{search_text.lower()}')]
+        try_school_uni_mean_yoy_basic_income_df.school.str.contains(f'{search_degree_text.lower()}')]
     if (query_df.shape[0] == 0):
         "No Records."
     else:
         # groupby degree and year
-        query_df = query_df.drop(query_df[query_df['basic_monthly_mean'] == 'na'].index) 
+        query_df = query_df.drop(query_df[query_df['basic_monthly_mean'] == 'na'].index)
         # query_df['employment_rate_overall_pct'] = (pd.to_numeric(query_df['employment_rate_overall'].str[:-1])
         #                             .div(100)
         #                     .mask(query_df['employment_rate_overall'] == '%', 0))
@@ -49,7 +50,6 @@ if search_text:
             query_deg_df_frame.key.eq(0) | query_deg_df_frame['basic_monthly_mean'].notna()]
         query_deg_df_frame = query_deg_df_frame.fillna(0)
 
-
         # groupby uni and year
         query_uni_df = query_df.groupby(["university", "year"]).mean().reset_index()
         # df for animated chart
@@ -61,11 +61,11 @@ if search_text:
         query_uni_df_frame = query_uni_df_frame[
             query_uni_df_frame.key.eq(0) | query_uni_df_frame['basic_monthly_mean'].notna()]
 
-        fig = px.treemap(query_df, 
-                 path=['university', 'degree'], 
-                 values='basic_monthly_mean',
-                 color='basic_monthly_mean'
-                )
+        fig = px.treemap(query_df,
+                         path=['university', 'degree'],
+                         values='basic_monthly_mean',
+                         color='basic_monthly_mean'
+                         )
 
         fig.update_layout(
             legend=dict(
@@ -86,12 +86,12 @@ if search_text:
 
         st.plotly_chart(fig, use_container_width=True)
 
-        # fig0 = px.treemap(query_df, 
-        #          path=['university', 'degree'], 
+        # fig0 = px.treemap(query_df,
+        #          path=['university', 'degree'],
         #          values='employment_rate_overall_pct',
         #          color='employment_rate_overall_pct'
         #     )
-                
+
         # fig0.update_layout(
         #     legend=dict(
         #         x=0,
@@ -110,7 +110,6 @@ if search_text:
         # )
 
         # st.plotly_chart(fig0, use_container_width=True)
-
 
         query_uni_df_frame = query_uni_df_frame.fillna(0)
 
